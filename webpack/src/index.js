cf092b0f09ccb991ce89f40407b724a569fa9737
Kevan <44446640+kevankas@users.noreply.github.com>
2023-08-26 01:22:40 -0500

    initial boilerplate

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -3,10 +3,21 @@ import _ from 'lodash';
  function component() {
    const element = document.createElement('div');
 
-  // Lodash, now imported by this script
+  
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
    return element;
  }
 
+
+
+ function makeElements()
+ {
+  const fillPage = document.getElementById("content");
+  
+  fillPage.innerHTML = ("testt");
+ }
+ 
+
  document.body.appendChild(component());
+ document.body.appendChild(makeElements());
